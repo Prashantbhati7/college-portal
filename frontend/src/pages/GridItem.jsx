d4de4ea77061f80a0b71a97b5d5355cd50362cc5
Prashant Bhati <prashant@Prashants-MacBook-Air.local>
2025-08-23 12:49:13 +0530

    initial commit

---
+++ frontend/src/pages/GridItem.jsx
@@ -0,0 +1,14 @@
+
+export default function GridItem({note}){
+return (
+<div className="bg-white p-4 py-7 mt-4 shadow-xl rounded-md">
+              <h2 className="text-xl font-semibold">{note.title}</h2>
+              <p className="text-[18px]">Subject: {note.subject}</p>
+              <p>Type: {note.Type}</p>
+              <a href={note.file} target="_blank" rel="noreferrer" className="text-blue-600 underline mt-2 inline-block">
+                View/Download
+              </a>
+              <p className="mt-4 text-slate-400">uploaded By:- {note.UploadedBy}</p>
+            </div>
+);
+}
